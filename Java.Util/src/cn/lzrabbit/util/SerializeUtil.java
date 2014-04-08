b352b9403c40066b023c3514cbf1b8207f176171
lzrabbit <lzrabbit@126.com>
2014-04-08 17:40:56 +0800

    xml序列化
    xml序列化过滤命名空间改为内部类实现

--- Java.Util/src/cn/lzrabbit/util/SerializeUtil.java
+++ Java.Util/src/cn/lzrabbit/util/SerializeUtil.java
@@ -16,10 +16,14 @@ import javax.xml.stream.XMLStreamWriter;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.validation.Schema;
 
+import org.aspectj.weaver.patterns.ThisOrTargetAnnotationPointcut;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.XMLWriter;
+import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLFilterImpl;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -48,6 +52,7 @@ public class SerializeUtil {
 	}
 
 	public static String toXML(Object obj) {
+		// http://stackoverflow.com/questions/277502/jaxb-how-to-ignore-namespace-during-unmarshalling-xml-document
 
 		try {
 			JAXBContext context = JAXBContext.newInstance(obj.getClass());
@@ -57,9 +62,9 @@ public class SerializeUtil {
 			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");// //编码格式
 			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);// 是否格式化生成的xml串
 			marshaller.setProperty(Marshaller.JAXB_FRAGMENT, false);// 是否省略xm头声明信息
-//			 StringWriter writer = new StringWriter();
-//			 marshaller.marshal(obj, writer);
-//			 return writer.toString();
+			// StringWriter writer = new StringWriter();
+			// marshaller.marshal(obj, writer);
+			// return writer.toString();
 
 			// marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new NamespacePrefixMapper() {
 			// @Override
@@ -80,14 +85,44 @@ public class SerializeUtil {
 			format.setNewlines(true);
 			format.setNewLineAfterDeclaration(false);
 			XMLWriter writer = new XMLWriter(out, format);
-			NamespaceFilter nsfFilter = new NamespaceFilter();
-			nsfFilter.setDefaultNamespace("xxoo.com");
+
+			XMLFilterImpl nsfFilter = new XMLFilterImpl() {
+				private boolean ignoreNamespace = false;
+				private String rootNamespace = null;
+				private boolean isRootElement = true;
+
+				@Override
+				public void startDocument() throws SAXException {
+					super.startDocument();
+				}
+
+				@Override
+				public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+					if (this.ignoreNamespace) uri = "";
+					if (this.isRootElement) this.isRootElement = false;
+					else if (!uri.equals("") && !localName.contains("xmlns")) localName = localName + " xmlns=\"" + uri + "\"";
+
+					super.startElement(uri, localName, localName, atts);
+				}
+
+				@Override
+				public void endElement(String uri, String localName, String qName) throws SAXException {
+					if (this.ignoreNamespace) uri = "";
+					super.endElement(uri, localName, localName);
+				}
+
+				@Override
+				public void startPrefixMapping(String prefix, String url) throws SAXException {
+					if (this.rootNamespace != null) url = this.rootNamespace;
+					if (!this.ignoreNamespace) super.startPrefixMapping("", url);
+
+				}
+			};
 			nsfFilter.setContentHandler(writer);
 			marshaller.marshal(obj, nsfFilter);
 			return out.toString();
 
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			throw new RuntimeException(e);
 		}
 	}
@@ -106,10 +141,33 @@ public class SerializeUtil {
 			JAXBContext context = JAXBContext.newInstance(valueType);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
 			// return (T) unmarshaller.unmarshal(new StringReader(xml));
-
+			SerializeUtil obj = new SerializeUtil();
 			XMLReader reader = XMLReaderFactory.createXMLReader();
-			NamespaceFilter nsfFilter = new NamespaceFilter();
-			nsfFilter.setIgnoreNamespace(true);
+			XMLFilterImpl nsfFilter = new XMLFilterImpl() {
+				private boolean ignoreNamespace = false;
+
+				@Override
+				public void startDocument() throws SAXException {
+					super.startDocument();
+				}
+
+				@Override
+				public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+					if (this.ignoreNamespace) uri = "";
+					super.startElement(uri, localName, qName, atts);
+				}
+
+				@Override
+				public void endElement(String uri, String localName, String qName) throws SAXException {
+					if (this.ignoreNamespace) uri = "";
+					super.endElement(uri, localName, localName);
+				}
+
+				@Override
+				public void startPrefixMapping(String prefix, String url) throws SAXException {
+					if (!this.ignoreNamespace) super.startPrefixMapping("", url);
+				}
+			};
 			nsfFilter.setParent(reader);
 			InputSource input = new InputSource(new StringReader(xml));
 			SAXSource source = new SAXSource(nsfFilter, input);
